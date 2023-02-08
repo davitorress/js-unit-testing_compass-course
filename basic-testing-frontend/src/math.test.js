15e6ce29eed0af0ef14824850316130bd6c94112
davitorress <dev.davitorres@gmail.com>
2023-02-08 14:47:29 -0300

    fix: AAA pattern to test

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,6 +3,11 @@ import { expect, it } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
-	const result = add([1, 2, 3]);
-	expect(result).toBe(6);
+	const numbers = [1, 2, 3]; // Arrange
+
+	const result = add(numbers); // Act
+
+	// Assert
+	const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + curValue, 0);
+	expect(result).toBe(expectedResult);
 });

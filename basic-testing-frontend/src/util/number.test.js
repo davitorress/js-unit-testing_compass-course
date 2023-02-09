c046a8439f58f1b3c1e7e4b71fbed62ab41f846b
davitorress <dev.davitorres@gmail.com>
2023-02-09 13:45:20 -0300

    test[refactor]: expect on array of string numbers

--- basic-testing-frontend/src/util/number.test.js
+++ basic-testing-frontend/src/util/number.test.js
@@ -34,7 +34,7 @@ describe("cleanNumbers()", () => {
 	it("should return an array of number values if an array of string number values is provided", () => {
 		const numberValues = ["1", "2"];
 		const result = cleanNumbers(numberValues);
-		expect(result[0]).toBeTypeOf("number");
+		expect(result).toEqual([1, 2]);
 	});
 
 	it("should throw an error if an array with at least one empty string is provided", () => {
